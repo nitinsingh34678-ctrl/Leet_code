32d3fb8fef02ca5f0aa18c57cba513f91827b169
Nitin Singh <nitinsingh34678@gmail.com>
2026-08-17 16:37:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (95.96%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
@@ -0,0 +1,11 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int i,k=1;
+    for(i=1;i<numsSize;i++){
+        if(nums[i]==nums[i-1]){
+            continue;
+        }
+        nums[k]=nums[i];        
+        k++;
+    }
+    return k;
+}
